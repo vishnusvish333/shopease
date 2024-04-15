39a3fa8bd78fe8391f8a10ec0e2048179a6b46ab
vishnuvish333 <vishnu.tvm777@gmail.com>
2024-04-15 13:01:59 +0530

    first commit

---
+++ shopease/shopease/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
+from .import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.home,name='home'),
+    path('store/',include('store.urls')),
+    path('cart/',include('carts.urls')),
+    path('accounts/',include('accounts.urls')),
+
+
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
